d2361f28ab2add8291ba4b2a41b2162c4f789cf4
JUNKI-LAPTOP\Junki <zxc7023@gmail.com>
2018-10-16 20:04:13 +0900

    성욱이형 오류 수정

--- groupware/src/main/java/com/kimsclub/groupware/dao/BoardDAO.java
+++ groupware/src/main/java/com/kimsclub/groupware/dao/BoardDAO.java
@@ -26,7 +26,8 @@ public class BoardDAO {
 	}
 	
 	public List<BoardVO> detail(){
-		return session.selectOne("board.selectDetail", arg1)
-		
+		//성욱이형 커밋을 잘하자 10번 외칩니다.
+		//return session.selectOne("board.selectDetail", arg1);
+		return null;
 	}
 }
