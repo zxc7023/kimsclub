e97d04d9454ef40a2e05a43ebd7a03436becdff8
kjg20260 <40974959+kjg20260@users.noreply.github.com>
2018-10-29 18:27:02 +0900

    결재선 팝업창 생성

--- groupware/src/main/java/com/kimsclub/groupware/service/ApprovalService.java
+++ groupware/src/main/java/com/kimsclub/groupware/service/ApprovalService.java
@@ -2,12 +2,18 @@ package com.kimsclub.groupware.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.kimsclub.groupware.vo.ApprovalLineVO;
 import com.kimsclub.groupware.vo.FormVO;
 
+@Service
 public interface ApprovalService {
 
 	List<FormVO> getUseFormlist();
 
 	String loadForm(int form_no);
+
+	List<ApprovalLineVO> loadMyApprovalLine(int employee_no);
 
 }
---
+++ groupware/src/main/java/com/kimsclub/groupware/vo/ApprovalLineVO.java
@@ -0,0 +1,24 @@
+package com.kimsclub.groupware.vo;
+
+public class ApprovalLineVO {
+	private int approval_path_no;
+	private String approval_path_name;
+	
+	
+	
+	
+	public int getApproval_path_no() {
+		return approval_path_no;
+	}
+	public void setApproval_path_no(int approval_path_no) {
+		this.approval_path_no = approval_path_no;
+	}
+	public String getApproval_path_name() {
+		return approval_path_name;
+	}
+	public void setApproval_path_name(String approval_path_name) {
+		this.approval_path_name = approval_path_name;
+	}
+	
+	
+}
